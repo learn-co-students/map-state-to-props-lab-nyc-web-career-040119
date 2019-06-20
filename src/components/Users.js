8ed7a578bcb0ab450388de62fa11a19a48819862
danfyfe <fyfedaniel@gmail.com>
2019-06-20 11:29:42 -0400

    Done.

--- src/components/Users.js
+++ src/components/Users.js
@@ -2,14 +2,15 @@ import React, { Component } from 'react';
 import {connect} from 'react-redux'
 class Users extends Component {
   renderUsers = () => {
-    console.log('inside renderUsers', this.props.users)
+    // console.log('inside renderUsers', this.props.users)
     return this.props.users.map(user=>{return <li>Username: {user.username} Hometown: {user.hometown}</li>})
   }
   render() {
-    console.log("from Users", this.props.users)
+    // console.log("from Users", this.props.users)
     return (
       <div>
         <ul>
+        <h1>{this.props.users.length}</h1>
           {this.renderUsers()}
         </ul>
       </div>
