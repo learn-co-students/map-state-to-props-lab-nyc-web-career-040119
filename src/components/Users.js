07a79167f802e12272aaec459b907991fded2b28
Robert O'Gara <rgo594@gmail.com>
2019-06-23 13:12:59 -0400

    Done.

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,18 +1,41 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
+
+
 
 class Users extends Component {
 
+//   handleOnClickItems() {
+//   this.props.store.dispatch({
+//     type: 'GET_COUNT_OF_ITEMS',
+//   });
+// }
+
+
   render() {
+    const displayUsers = this.props.users.map(user => {
+      return <li key={user.username}>{user.username}</li>
+    })
+    const displayUserCount = this.props.userCount
+
     return (
       <div>
         <ul>
-          Users!
+        {this.props.userCount}
+        {displayUsers}
         </ul>
       </div>
     )
   }
 }
 
-//add mapStateToProps here
+const mapStateToProps = (state) => {
+  // state.users
+  return {
+    users: state.users,
+    userCount: state.users.length
+  }
+}
 
-export default Users
+export default connect(mapStateToProps)(Users);
+// export default Users;
